0f9201332f587760cf34a3074e8869ce8c409479
ahaugedk <ahauge@orderly.ai>
2020-09-09 15:22:17 +0200

    Reset redux store and more tests.

--- test/time_format_widget_test.dart
+++ test/time_format_widget_test.dart
@@ -3,9 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:intl/intl.dart';
 import 'package:mockito/mockito.dart';
 import 'package:world_clock_app/async_redux/action/change_format_action.dart';
+import 'package:world_clock_app/async_redux/connector/time_connector.dart';
+import 'package:world_clock_app/async_redux/store/app_state.dart';
 import 'package:world_clock_app/async_redux/store/redux_store.dart';
+import 'package:world_clock_app/bloc/city_time_bloc.dart';
 import 'package:world_clock_app/ui/city_time_screen.dart';
-import 'package:world_clock_app/ui/dummy_widget.dart';
 import 'package:http/http.dart' as http;
 import 'package:world_clock_app/util/http_client.dart' as httpClient;
 
@@ -14,6 +16,31 @@ import 'build_test_main.dart';
 class MockClient extends Mock implements http.Client {}
 
 void main(){
+
+  setUp((){
+    store.defineState(AppState.initialState());
+  });
+
+  testWidgets('Time Connector Test', (WidgetTester tester) async {
+    var time = DateTime(2020, 13, 00, 00);
+    await tester.pumpWidget(buildTestMain(TimeConnector(time)));
+    await tester.pumpAndSettle();
+    
+    var timeStr = DateFormat.jm().format(time);
+    expect(
+      find.text(timeStr),
+      findsOneWidget
+    );
+
+    await store.dispatchFuture(ChangeTimeFormatAction());
+    await tester.pumpAndSettle();
+    
+    var time24H = DateFormat.Hm().format(time);
+    expect(
+      find.text(time24H),
+      findsOneWidget
+    );
+  });
 
   testWidgets('City Time Test', (WidgetTester tester) async {  
     httpClient.http = MockClient();
@@ -46,5 +73,19 @@ void main(){
       findsOneWidget
     );
 
+  });
+
+  test("City Time Bloc", () async {
+    httpClient.http = MockClient();
+
+    when(httpClient.http.get('http://worldtimeapi.org/api/timezone/Europe/Copenhagen'))
+          .thenAnswer((_) async => http.Response('{"abbreviation":"CEST","client_ip":"2.104.7.182","datetime":"2020-09-09T14:12:46.794845+02:00","day_of_week":3,"day_of_year":253,"dst":true,"dst_from":"2020-03-29T01:00:00+00:00","dst_offset":3600,"dst_until":"2020-10-25T01:00:00+00:00","raw_offset":3600,"timezone":"Europe/Copenhagen","unixtime":1599653566,"utc_datetime":"2020-09-09T12:12:46.794845+00:00","utc_offset":"+02:00","week_number":37}', 200));
+
+    var bloc = CityTimeBloc(url: "Europe/Copenhagen");
+    var initial = await bloc.loadInitialState();
+    var time = initial.timeNow();
+    expect(time.hour, 14); 
+    expect(time.minute, 12); 
+    expect(time.second, 46); 
   });
 }
