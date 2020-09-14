abd8db8669610adc7bb3248afacc7e413f7a64de
Martin Afani <martin@orderly.ai>
2020-09-14 14:33:36 +0300

    Add test for the ThemeModeAction

--- test/action/theme_mode_action_test.dart
+++ test/action/theme_mode_action_test.dart
@@ -1,5 +1,4 @@
 import 'package:async_redux/async_redux.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:world_clock_app/async_redux/action/theme_toggle_action.dart';
 import 'package:world_clock_app/async_redux/store/app_state.dart';
