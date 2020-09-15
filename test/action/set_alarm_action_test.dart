b8c497ab36f5a445a5508bb7d976947a8f2ba1ef
Martin Afani <martin@orderly.ai>
2020-09-15 10:03:49 +0300

    Add test for the SetAlarmAction

---
+++ test/action/set_alarm_action_test.dart
@@ -0,0 +1,17 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:world_clock_app/async_redux/action/set_alarm_action.dart';
+import 'package:world_clock_app/async_redux/store/app_state.dart';
+
+void main() {
+  test('Test SetAlarmAction', () async {
+    var timeOfDay = TimeOfDay.now();
+    var storeTester = StoreTester(initialState: AppState.initialState());
+    storeTester
+        .dispatch(SetAlarmAction(url: 'Africa/Kampala', alarm: timeOfDay));
+
+    TestInfo<AppState> info = await storeTester.wait(SetAlarmAction);
+    expect(info.state.alarm, timeOfDay);
+  });
+}
