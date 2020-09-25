fc189264847385ce5c7a4cf349a47b61ff15d67d
Martin Afani <martin@orderly.ai>
2020-09-25 22:25:14 +0300

    Clean up the test suites

--- test/action/set_alarm_action_test.dart
+++ test/action/set_alarm_action_test.dart
@@ -2,10 +2,7 @@ import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:world_clock_app/async_redux/action/set_alarm_action.dart';
-import 'package:world_clock_app/async_redux/connector/set_alarm_connector.dart';
 import 'package:world_clock_app/async_redux/store/app_state.dart';
-
-import '../widget/build_test_main.dart';
 
 void main() {
   test('Test SetAlarmAction', () async {
@@ -17,12 +14,5 @@ void main() {
 
     TestInfo<AppState> info = await storeTester.wait(SetAlarmAction);
     expect(info.state.alarm, timeOfDay);
-  });
-
-  testWidgets('Test scheduled alarm', (WidgetTester tester) async {        
-    await tester.pumpWidget(buildTestMain(SetAlarmConnector()));
-    await tester.pumpAndSettle();
-
-    expect(find.text('Africa/Cairo'), findsOneWidget);
   });
 }
--- test/widget/city_time_screen_test.dart
+++ test/widget/city_time_screen_test.dart
@@ -10,43 +10,37 @@ import 'package:world_clock_app/ui/alarms_screen.dart';
 import 'package:world_clock_app/ui/city_time_screen.dart';
 import 'build_test_main.dart';
 
-class MockTimezoneRepository implements ITimezoneRepository
-{
+class MockTimezoneRepository implements ITimezoneRepository {
   @override
-  Future<TimezoneInfo> getTimezoneInfo(String timezone) async
-  {
-    if (timezone == "Europe/Copenhagen")
-    {      
+  Future<TimezoneInfo> getTimezoneInfo(String timezone) async {
+    if (timezone == "Europe/Copenhagen") {
       return TimezoneInfo(
-        abbreviation: "CEST",
-        clientIp: "2.104.7.182",
-        datetime: "2020-09-09T14:12:46.794845+02:00",
-        dayOfWeek: 3,
-        dayOfYear: 253,
-        dst: true,
-        dstFrom: "2020-03-29T01:00:00+00:00",
-        dstUntil: "2020-10-25T01:00:00+00:00",
-        rawOffset: 3600,
-        timezone: "Europe/Copenhagen",
-        unixtime: 1599653566,
-        utcDatetime: "2020-09-09T12:12:46.794845+00:00",
-        utcOffset: "+02:00",
-        weekNumber: 37
-      );
+          abbreviation: "CEST",
+          clientIp: "2.104.7.182",
+          datetime: "2020-09-09T14:12:46.794845+02:00",
+          dayOfWeek: 3,
+          dayOfYear: 253,
+          dst: true,
+          dstFrom: "2020-03-29T01:00:00+00:00",
+          dstUntil: "2020-10-25T01:00:00+00:00",
+          rawOffset: 3600,
+          timezone: "Europe/Copenhagen",
+          unixtime: 1599653566,
+          utcDatetime: "2020-09-09T12:12:46.794845+00:00",
+          utcOffset: "+02:00",
+          weekNumber: 37);
     }
 
     throw UnimplementedError();
   }
-  
-    @override
-    Future<TimezoneList> getTimezoneList(String region) {
-    // TODO: implement getTimezoneList
+
+  @override
+  Future<TimezoneList> getTimezoneList(String region) {
     throw UnimplementedError();
   }
 }
 
 void main() {
-
   setUp(() {
     store.defineState(AppState.initialState());
   });
@@ -54,13 +48,10 @@ void main() {
   testWidgets('find and tap on set alarm button', (WidgetTester tester) async {
     expect(store.state.alarm == null, true);
 
-    await tester
-      .pumpWidget(buildTestMain(CityTime("Europe/Copenhagen"), 
+    await tester.pumpWidget(buildTestMain(CityTime("Europe/Copenhagen"),
         repositoryProvider: RepositoryProvider<ITimezoneRepository>(
           create: (_) => MockTimezoneRepository(),
-        )
-      )
-    );
+        )));
     await tester.pumpAndSettle();
 
     //Verify that alarms screen is empty
@@ -70,7 +61,7 @@ void main() {
     await tester.pumpAndSettle();
     expect(find.byType(AlarmsScreen), findsOneWidget);
     expect(find.text("No alarms"), findsOneWidget);
-    
+
     await tester.tap(find.byType(BackButton));
     await tester.pumpAndSettle();
 
@@ -79,21 +70,19 @@ void main() {
     await tester.tap(find.byType(RaisedButton));
     await tester.pumpAndSettle();
 
-    expect(find.text("SELECT TIME"), findsOneWidget);   
+    expect(find.text("SELECT TIME"), findsOneWidget);
     expect(find.byType(FlatButton), findsNWidgets(2));
-    
+
     await tester.tap(find.byType(FlatButton).last);
     await tester.pumpAndSettle();
 
     expect(store.state.alarm != null, true);
 
     //Verify alarm is visible in alarms screen
-        
+
     await tester.tap(gotoAlarmsButton);
     await tester.pumpAndSettle();
     expect(find.byType(AlarmsScreen), findsOneWidget);
     expect(find.text("Europe/Copenhagen"), findsOneWidget);
-
-
   });
 }
